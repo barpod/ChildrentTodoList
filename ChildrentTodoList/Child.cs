913975bd6024eeeb0c4130cfdd7741c116715197
Bartosz Podlejski <bartosz.podlejski@gmail.com>
2021-02-28 13:31:11 +0100

    Working on integration test

--- ChildrentTodoList/Child.cs
+++ ChildrentTodoList/Child.cs
@@ -1,28 +1,2 @@
 
-public class Child
-{
-    public string Id { get; }
-    public string FirstName { get; }
-    public string LastName { get; }
-
-    public Child(string id, string firstName, string lastName)
-    {
-        if (string.IsNullOrWhiteSpace(id))
-        {
-            throw new System.ArgumentException($"'{nameof(id)}' cannot be null or whitespace.", nameof(id));
-        }
-
-        if (string.IsNullOrWhiteSpace(firstName))
-        {
-            throw new System.ArgumentException($"'{nameof(firstName)}' cannot be null or whitespace.", nameof(firstName));
-        }
-
-        if (string.IsNullOrWhiteSpace(lastName))
-        {
-            throw new System.ArgumentException($"'{nameof(lastName)}' cannot be null or whitespace.", nameof(lastName));
-        }
-        Id = id;
-        FirstName = firstName;
-        LastName = lastName;
-    }
-}
+public record Child(string Id, string FirstName, string LastName);
